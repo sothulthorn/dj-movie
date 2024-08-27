b5e31a13f87630f14b904cb634f5046453af4572
sothulthorn <thornsothul@gmail.com>
2024-08-27 21:44:48 +0900

    Creating JSON Response - Individual Elements

--- movielist_app/views.py
+++ movielist_app/views.py
@@ -6,4 +6,13 @@ def movie_list(request):
   movies = Movie.objects.all()
   data = {'movies': list(movies.values())}
   return JsonResponse(data)
+
+def movie_details(request, pk):
+  movie = Movie.objects.get(pk=pk)
+  data = {
+    'name': movie.name,
+    'description': movie.description,
+    'active': movie.active,
+  }
+  return JsonResponse(data)
   
