182331baec96be83e4b8d2803d6a66d48319b412
sothulthorn <thornsothul@gmail.com>
2024-08-28 21:23:31 +0900

    APIView Class

--- movielist_app/api/urls.py
+++ movielist_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from movielist_app.api.views import movie_list, movie_details
+# from movielist_app.api.views import movie_list, movie_details
+from movielist_app.api.views import MovieListAV, MovieDetailsAV
 
 urlpatterns = [
-    path("list/", movie_list, name="movie-list"),
-    path("<int:pk>", movie_details, name='movie-details')
+    path("list/", MovieListAV.as_view(), name="movie-list"),
+    path("<int:pk>", MovieDetailsAV.as_view(), name='movie-details')
 ]
